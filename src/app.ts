6eeec02b4d82723487159ad2343e53bbda654c0f
Your Name <you@example.com>
2026-05-18 14:39:05 +0530

    Update CORS for production

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import profileRouter from "./routers/profile";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://task-manager-web-sable.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
